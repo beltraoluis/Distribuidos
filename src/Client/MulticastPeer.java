b4ebbcee2755d10b139ec8683ad3eda355bd4d4d
Luís Henrique Beltrão Santana <luis.2012@alunos.utfpr.edu.br>
2019-08-28 17:08:30 -0300

    Add files via upload

--- src/Client/MulticastPeer.java
+++ src/Client/MulticastPeer.java
@@ -1,27 +1,41 @@
 import java.net.*;
 import java.io.*;
 public class MulticastPeer{
-    public static void main(String args[]){ 
-        Boolean flag = true;
-        // args give message contents and destination multicast group (e.g. "228.5.6.7")
-        MulticastSocket s =null;
+    private Boolean flag = true;
+    // args give message contents and destination multicast group (e.g. "228.5.6.7")
+    MulticastSocket s = null;
+    
+    public MulticastPeer(String args[]){ 
         try {
             InetAddress group = InetAddress.getByName(args[1]);
-            s = new MulticastSocket(6789);
+            this.s = new MulticastSocket(6789);
             s.joinGroup(group);
             byte [] m = args[0].getBytes();
             DatagramPacket messageOut = new DatagramPacket(m, m.length, group, 6789);
             s.send(messageOut); 
             byte[] buffer = new byte[1000];
-            for(int i=0; i< 3;i++) {        // get messages from others in group
-                DatagramPacket messageIn = new DatagramPacket(buffer, buffer.length);
-                s.receive(messageIn);
-                System.out.println("Received:" + new String(messageIn.getData()));
-            }
+            new Thread(){
+                @Override
+                public void run(){
+                    while(flag) {        // get messages from others in group
+                        try{
+                            DatagramPacket messageIn = new DatagramPacket(buffer, buffer.length);
+                            s.receive(messageIn);
+                            System.out.println("Received:" + new String(messageIn.getData()));
+                        }catch(Exception e){
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            }.start();
             s.leaveGroup(group);        
         }catch (SocketException e){System.out.println("Socket: " + e.getMessage());
         }catch (IOException e){System.out.println("IO: " + e.getMessage());
         }finally {if(s != null) s.close();}
-    }               
+    }  
+        
+    public void stop(){
+        flag = false;
+    }             
     
 }
